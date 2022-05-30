88a1ab56508edc56879f8fbd50015ce05fbab314
Honghao Zeng <nat@nat.moe>
2022-05-30 01:33:04 -0700

    trace: cksum & byte order fixes

--- trace.c
+++ trace.c
@@ -6,20 +6,42 @@
 #include "trace.h"
 #include "log.h"
 
+uint16_t cksum(uint8_t* data, size_t length) {
+    uint32_t acc = 0xffff;
+
+    for (size_t i=0; i + 1 < length; i += 2) {
+        uint16_t word;
+        memcpy(&word, data + i, 2);
+        acc += ntohs(word);
+        if (acc > 0xffff) {
+            acc -= 0xffff;
+        }
+    }
+
+    if (length & 0x01) {
+        uint16_t word = 0;
+        memcpy(&word, data + length - 1, 1);
+        acc += ntohs(word);
+        if (acc > 0xffff) {
+            acc -= 0xffff;
+        }
+    }
+
+    return htons(~acc);
+}
+
 ssize_t build_rfc4950(const stack_t* stack, uint8_t *buffer, size_t bufsz) {
     uint8_t *ptr = buffer;
 
-    icmp_ext_hdr_t *exthdr = (icmp_ext_hdr_t *) ptr;
+    uint32_t *exthdr = (uint32_t *) ptr;
 
-    if (sizeof(icmp_ext_hdr_t) > bufsz) {
+    if (sizeof(uint32_t) > bufsz) {
         return -EBUFOSZ;
     }
 
-    exthdr->ver = 2;
-    exthdr->reserved = 0;
-    exthdr->cksum = 0;
+    *exthdr = htonl(2 << 28);
     
-    ptr += sizeof(icmp_ext_hdr_t);
+    ptr += sizeof(uint32_t);
 
     icmp_ext_obj_hdr_t *objhdr = (icmp_ext_obj_hdr_t *) ptr;
 
@@ -47,7 +69,7 @@ ssize_t build_rfc4950(const stack_t* stack, uint8_t *buffer, size_t bufsz) {
         ++lse_count;
     }
 
-    objhdr->length = sizeof(icmp_ext_obj_hdr_t) + lse_count * sizeof(uint32_t);
+    objhdr->length = htons(sizeof(icmp_ext_obj_hdr_t) + lse_count * sizeof(uint32_t));
 
     return (ptr - buffer);
 }
@@ -93,8 +115,8 @@ ssize_t build_reply(const hop_t *hops, size_t nhops, const uint8_t* inpkt, size_
 
     memset(optr, 0, sizeof(icmphdr_t));
 
-    oicmphdr->code = ICMP_TIME_EXCEEDED;
-    oicmphdr->type = ICMP_EXC_TTL;
+    oicmphdr->type = ICMP_TIME_EXCEEDED;
+    oicmphdr->code = ICMP_EXC_TTL;
     oicmphdr->length = ORIG_PAYLOAD_SZ;
 
     optr += sizeof(icmphdr_t);
@@ -103,7 +125,16 @@ ssize_t build_reply(const hop_t *hops, size_t nhops, const uint8_t* inpkt, size_
         return -EBUFOSZ;
     }
 
-    memcpy(optr, inpkt, 4 * ORIG_PAYLOAD_SZ);
+    size_t copy_sz = 4 * ORIG_PAYLOAD_SZ;
+    if (copy_sz > insz) {
+        copy_sz = insz;
+    }
+
+    memcpy(optr, inpkt, copy_sz);
+
+    if (copy_sz < 4 * ORIG_PAYLOAD_SZ) {
+        memset(optr + copy_sz, 0, 4 * ORIG_PAYLOAD_SZ - copy_sz);
+    }
 
     optr += 4 * ORIG_PAYLOAD_SZ;
 
@@ -115,7 +146,14 @@ ssize_t build_reply(const hop_t *hops, size_t nhops, const uint8_t* inpkt, size_
 
     optr += rfc4950_len;
 
-    return (optr - outpkt);
+    oicmphdr->checksum = cksum(((uint8_t *) oicmphdr), optr - (uint8_t *) oicmphdr);
+
+    size_t tot_len = (optr - outpkt);
+
+    ohdr->tot_len = htons(tot_len);
+    ohdr->check = cksum(outpkt, tot_len);
+
+    return tot_len;
 }
 
 int load_config(const char *config_file, hop_t **hops, size_t *nhops) {
@@ -137,7 +175,7 @@ int load_config(const char *config_file, hop_t **hops, size_t *nhops) {
 
     *hops = _hops;
     
-    while (fscanf(file, "%16s %2048s\n", addr, stacksdef) == 2) {
+    while (fscanf(file, "%16s %2048s", addr, stacksdef) == 2) {
         log_debug("input hop %zu: %s, input stack: %s\n", ln, addr, stacksdef);
         
         ++ln;
@@ -153,17 +191,19 @@ int load_config(const char *config_file, hop_t **hops, size_t *nhops) {
         for (stackdef = strtok_r(stacksdef, ",", &saveptr); ; stackdef = strtok_r(NULL, ",", &saveptr)) {
             if (stackdef == NULL) {
                 if (head == NULL) {
+                    log_fatal("no stack defined for hop %s\n", addr);
                     ret = -EPARSE;
                     goto end;
                 }
 
+                log_debug("end building stack for hop %s.\n", addr);
                 break;
             }
 
             sscanf(stackdef, "%u:%hhu:%hhu:%hhu", &label, &exp, &s, &ttl);
 
             stack_t *stack = (stack_t *) malloc(sizeof(stack_t));
-            stack->value = htonl(label) | exp << 9 | s << 8 | ttl;
+            stack->value = htonl(label << 12 | exp << 9 | s << 8 | ttl);
             stack->next = NULL;
 
             if (head == NULL) {
@@ -181,6 +221,8 @@ int load_config(const char *config_file, hop_t **hops, size_t *nhops) {
 
         _hops[_nhops].address = inet_addr(addr);
         _hops[_nhops].stack = head;
+
+        log_debug("loaded hop %zu.\n", _nhops);
         ++_nhops;
     }
 
